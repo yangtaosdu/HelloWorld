e1a45444fdbf507d69fa4dd4869df11ae5523b24
yangtao <yangtaosdu@163.com>
2017-09-03 23:51:29 +0800

    add readme

--- src/com/yangtao/test/HelloWorld.java
+++ src/com/yangtao/test/HelloWorld.java
@@ -1,7 +1,13 @@
 package com.yangtao.test;
 
+import com.yangtao.test.entry.Person;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! I am yangtao");
+        Person yangtao=new Person();
+        yangtao.setName("yangtao");
+        yangtao.setAge(20);
+        System.out.println("Name: "+ yangtao.getName()+" Age: "+yangtao.getAge());
     }
 }
